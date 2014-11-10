2dde608b051fea7a2006ad485eaf8f7347fff47e
mao8 <thisisma08@gmail.com>
2014-11-10 21:47:44 +0530

    Moved tcg code from translator

--- ass4_12CS30035_test.c
+++ ass4_12CS30035_test.c
@@ -1,10 +1,24 @@
-int prints(char *c);
-int main(){
-char k,c='s',*d;
-d = &c;
-char a[2];
-a[0]=*d;
-a[1]=0;
-prints(a);
-return 0;
+int printi(int n);
+int prints(char* c);
+int readi(int* eP);
+int fib(int num);
+ 
+int main() {
+  char *c="\nPlease Enter number: ";
+  char *d="\nThe fibonacci output is: ";
+  char *n="\n";
+  prints(c);
+   int err,num,i,j,k;
+    num = readi(&err);
+    prints(d);
+    printi(fib(num));
+    prints(n);
+  return 0;
+
+}
+ 
+int fib(int num) {
+    if(num <= 1) return 1;
+    else return fib(num-1)+fib(num-2);
+
 }
